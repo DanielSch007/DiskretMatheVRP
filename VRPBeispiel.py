cd0d0271e1273a3c9813c253cb0047bb1447628d
S0577771@htw-Berlin.de <S0577771@htw-Berlin.de>
2024-07-03 14:10:19 +0200

    richtiger code mit Komentaren

--- VRPBeispiel.py
+++ VRPBeispiel.py
@@ -1,5 +1,6 @@
 import requests
 import folium
+import geopy.distance
 
 class CVRP:
     def __init__(self, depot, customers, distances, demands, capacity, num_vehicles):
@@ -11,23 +12,23 @@ class CVRP:
         self.num_vehicles = num_vehicles
         self.routes = [[depot, customer, depot] for customer in customers]
     
-    # auf Folie 22 erklärt
+    #einsparung berechnen (Folie 22) von jeder möglichen Kante
     def calculate_savings(self):
         savings = {}
-        for i in self.customers: #einsparung vo jeder möglichen kante berechnen
+        for i in self.customers:
             for j in self.customers:
                 if i != j:
                     savings[(i, j)] = self.distances[self.depot][i] + self.distances[self.depot][j] - self.distances[i][j]
         return sorted(savings.items(), key=lambda item: item[1], reverse=True)
     
-    # auf Folie 24 erklärt
+    #Routen kombinieren wenn die Nebenbedingungen es zulassen (Folie 24)
     def merge_routes(self, savings):
         for (i, j), saving in savings:
             route_i = next((route for route in self.routes if i in route), None)
             route_j = next((route for route in self.routes if j in route), None)
             if route_i and route_j and route_i != route_j:
                 total_demand = sum(self.demands[node] for node in route_i[1:-1] + route_j[1:-1])
-                if total_demand <= self.capacity: # wenn Kapazität erlaubt neue Route
+                if total_demand <= self.capacity:
                     new_route = route_i[:-1] + route_j[1:]
                     self.routes.remove(route_i)
                     self.routes.remove(route_j)
@@ -37,8 +38,8 @@ class CVRP:
         savings = self.calculate_savings()
         self.merge_routes(savings)
         return self.routes[:self.num_vehicles]
-
-# Daten aus Overpass API (https://python-overpy.readthedocs.io/en/latest/)
+    
+# Overpass API (https://python-overpy.readthedocs.io/en/latest/) für Map Daten
 def get_osm_restaurants(area_name):
     overpass_url = "http://overpass-api.de/api/interpreter"
     overpass_query = f"""
@@ -59,32 +60,43 @@ def get_osm_restaurants(area_name):
     
     return restaurants
 
-# Beispielproblem für Treptow-Köpenick
-depot = (52.41667, 13.56667)  # Depot-Koordinaten (Beispiel)
-customers = [(52.437, 13.568), (52.412, 13.565), (52.430, 13.572), (52.418, 13.563)]  # Kunden (Beispiel)
-distances = {
-    depot: {cust: 0 for cust in customers},
-    **{cust: {cust2: 0 for cust2 in customers} for cust in customers}
-}  # Distanzen (Beispiel, alle Distanzen sind 0)
-demands = {cust: 1 for cust in customers}  # Bedarfe (Beispiel, alle Bedarfe sind 1)
-capacity = 4  # Kapazität (Beispiel)
-num_vehicles = 2  # Anzahl Fahrzeuge (Beispiel)
+# Distanz berechnung mit geopy
+def calculate_distance_matrix(depot, customers):
+    locations = [depot] + customers
+    distances = {}
+    for loc1 in locations:
+        distances[loc1] = {}
+        for loc2 in locations:
+            if loc1 == loc2:
+                distances[loc1][loc2] = 0
+            else:
+                distances[loc1][loc2] = geopy.distance.distance(loc1, loc2).km
+    return distances
 
-# VRP lösen
-cvrp = CVRP(depot, customers, distances, demands, capacity, num_vehicles)
-routes = cvrp.solve()
+# HTW-Berlin als Depot
+depot = (52.45731, 13.52637)  # HTW Berlin Koordinaten
 
 # Restaurants aus OpenStreetMap abrufen (Treptow-Köpenick)
 area_name = 'Treptow-Köpenick'
 restaurants = get_osm_restaurants(area_name)
-print("Restaurants:", restaurants)  # Debugging-Ausgabe
+customers = [tuple(rest['location']) for rest in restaurants]
+demands = {customer: 1 for customer in customers}  # Beispiel: alle Restaurants haben Bedarf von 1
+capacity = 4  # Beispiel: Kapazität eines Fahrzeugs
+num_vehicles = 3  # Beispiel: Anzahl der Fahrzeuge
+
+# Distanzmatrix berechnen
+distances = calculate_distance_matrix(depot, customers)
+
+# VRP lösen
+cvrp = CVRP(depot, customers, distances, demands, capacity, num_vehicles)
+routes = cvrp.solve()
 
 # Karte erstellen und Depot, Restaurants, Routen einzeichnen
 map_center = depot  # Zentrum der Karte auf das Depot setzen
 map_osm = folium.Map(location=map_center, zoom_start=13)
 
 # Depot markieren
-folium.Marker(location=depot, popup='Depot', icon=folium.Icon(color='blue')).add_to(map_osm)
+folium.Marker(location=depot, popup='Depot (HTW Berlin)', icon=folium.Icon(color='blue')).add_to(map_osm)
 
 # Restaurants einzeichnen
 for restaurant in restaurants:
